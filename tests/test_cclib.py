b9eecbf81e40664ebb08a6df36d67d39de6ee907
Dimitris Verraros <vervas.g@gmail.com>
2014-05-08 13:19:39 +0200

    Fix failing test

--- tests/test_cclib.py
+++ tests/test_cclib.py
@@ -6,8 +6,7 @@ class TestAPI(TestCase):
     def test_token_source_url_fallback(self):
         self.assertEquals('my.tokensource.url',
                           API(url='any.url', token_source_url='my.tokensource.url').token_source_url)
-        self.assertEquals('my.api.url', API(url='my.api.url').token_source_url)
+        self.assertEquals('my.api.url/token/', API(url='my.api.url').token_source_url)
 
     def test_create_token_with_tokensouce_url(self):
         API(url='any.url', token_source_url='my.tokensource.url')
-
